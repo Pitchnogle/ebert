0b3f5d8cc373dd5e9d03b58a711162b4d8e35822
Pitchnogle <pitchnogle@gmail.com>
2023-11-05 15:16:19 -0700

    change server destination folder

--- sync_music.bat
+++ sync_music.bat
@@ -3,7 +3,9 @@
 :: Written by: Justin Hadella (pitchnogle@gmail.com)
 
 set src_dir="C:\Users\justi\Music\Music Center"
-set dst_dir="\\ebert\music"
+set dst_dir="\\ebert\cd"
 
 :: Copy all (newer) files from src_dir to dst_dir
 xcopy %src_dir% %dst_dir% /v /s /e /d /y
+
+@REM forfiles /p %src_dir% /s /m *.flac /c "cmd /c echo @relpath -- @fname"
