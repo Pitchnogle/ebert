9465894725525275e5d9c62adbe8bbe789dea937
Pitchnogle <pitchnogle@gmail.com>
2023-11-05 15:16:03 -0700

    add script

---
+++ sync_aac_archive.bat
@@ -0,0 +1,11 @@
+:: Batch script to synchronize Sony Music Center content with the NAS
+::
+:: Written by: Justin Hadella (pitchnogle@gmail.com)
+
+set src_dir="C:\Users\justi\Music\aac_archive"
+set dst_dir="\\ebert\music"
+
+:: Copy all (newer) files from src_dir to dst_dir
+xcopy %src_dir% %dst_dir% /v /s /e /d /y
+
+@REM forfiles /p %src_dir% /s /m *.flac /c "cmd /c echo @relpath -- @fname"
